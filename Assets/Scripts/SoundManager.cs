42d291f403fb8dc09ec5b8ea491d4e60816f406b
Jacob Cheatley (AWARE) <jacob.cheatley@awaregroup.com>
2022-01-30 10:45:29 +1300

    Add Sound Manager and music

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundManager : MonoBehaviour
+{
+    [Header("Audio Mixer")]
+    [SerializeField]
+    private AudioMixer audioMixer;
+    [SerializeField]
+    private AudioMixerSnapshot[] snapshots;
+
+    [SerializeField]
+    private AudioSource sfxSource;
+
+    private SoundManager instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+
+    public void TransitionMusic(int snapshotIndex, float timeToReach)
+    {
+        instance.audioMixer.TransitionToSnapshots(new AudioMixerSnapshot[] { instance.snapshots[snapshotIndex] }, new float[] { 1 }, timeToReach);
+    }
+
+    public void PlayClip(AudioClip clip, float volume = 1f)
+    {
+        instance.sfxSource.PlayOneShot(clip, volume);
+    }
+}
